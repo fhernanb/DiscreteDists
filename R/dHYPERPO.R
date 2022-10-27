064a6dbba46b7e7e5fbf272627f2bd033789ec0a
fhernanb <fhernanb@unal.edu.co>
2022-10-27 08:51:51 -0500

    New functions

--- R/dHYPERPO.R
+++ R/dHYPERPO.R
@@ -2,13 +2,15 @@
 #'
 #' @description
 #' Those functions define the density, distribution function, quantile
-#' function and random generation for the Poisson, PO(), distribution
+#' function and random generation for the hyper-Poisson, HYPERPO(), distribution
 #' with parameters \code{mu} and \code{sigma}.
 #'
-#' @param x vector of (non-negative integer) quantiles.
+#' @param x,q vector of (non-negative integer) quantiles.
 #' @param mu vector of positive values of this parameter.
 #' @param sigma vector of positive values of this parameter.
-#' @param log logical; if TRUE, probabilities p are given as log(p).
+#' @param n number of random values to return
+#' @param log,log.p logical; if TRUE, probabilities p are given as log(p).
+#' @param lower.tail logical; if TRUE (default), probabilities are \code{P[X <= x]}, otherwise, \code{P[X > x]}.
 #'
 #' @return
 #' \code{dHYPERPO} gives the density, \code{pHYPERPO} gives the distribution
@@ -38,4 +40,56 @@ dHYPERPO <- function(x, mu=1, sigma=1, log=FALSE){
     return(res)
   else
     return(exp(res))
+}
+#' @export
+#' @rdname dHYPERPO
+pHYPERPO <- function(q, mu=1, sigma=1, lower.tail = TRUE, log.p = FALSE){
+  if (any(sigma <= 0))  stop("parameter gamma has to be positive!")
+  if (any(mu <= 0)) stop("parameter lambda has to be positive!")
+  if (any(q < 0)) stop(paste("x must be >=0", "\n", ""))
+  ly <- max(length(q), length(mu), length(sigma))
+  q <- rep(q, length = ly)
+  mu <- rep(mu, length = ly)
+  sigma <- rep(sigma, length = ly)
+  # Begin auxiliar function
+  aux_func <- function(q, mu, sigma) {
+    cdf <- numeric(length(q))
+    for (i in 1:length(q)) {
+      res <- dHYPERPO(x=0:q[i], mu=mu[i], sigma=sigma[i], log=FALSE)
+      cdf[i] <- sum(res)
+    }
+    cdf
+  }
+  # End auxiliar function
+  cdf <- aux_func(q=q, mu=mu, sigma=sigma)
+  if (lower.tail == TRUE)
+    cdf <- cdf
+  else cdf = 1 - cdf
+  if (log.p == FALSE)
+    cdf <- cdf
+  else cdf <- log(cdf)
+  cdf
+}
+#' @importFrom stats runif
+#' @export
+#' @rdname dHYPERPO
+rHYPERPO <- function(n, mu, sigma) {
+  if (any(sigma <= 0))  stop("parameter gamma has to be positive!")
+  if (any(mu <= 0)) stop("parameter lambda has to be positive!")
+  if (any(n <= 0)) stop(paste("n must be a positive integer", "\n", ""))
+  # Begin auxiliar function
+  one_hyperpo <- function(u, mu, sigma) {
+    p <- dHYPERPO(x=0, mu=mu, sigma=sigma, log=FALSE)
+    F <- p
+    i <- 0
+    while (u >= F) {
+      i <- i + 1
+      p <- dHYPERPO(x=i, mu=mu, sigma=sigma, log=FALSE)
+      F <- F + p
+    }
+    return(i)
+  }
+  one_hyperpo <- Vectorize(one_hyperpo)
+  # End auxiliar function
+  one_hyperpo(runif(n), mu, sigma)
 }
--- examples/examples_dHYPERPO.R
+++ examples/examples_dHYPERPO.R
@@ -13,3 +13,42 @@ legend("topright", col=c("dodgerblue", "tomato", "green4"), lwd=3,
        legend=c("sigma=0.01, mu=5",
                 "sigma=0.50, mu=5",
                 "sigma=1.00, mu=5"))
+
+# Example 2
+# Checking if curves go to 1
+x_max <- 15
+cumulative_probs1 <- pHYPERPO(q=0:x_max, mu=1, sigma=1.5)
+cumulative_probs2 <- pHYPERPO(q=0:x_max, mu=3, sigma=1.5)
+cumulative_probs3 <- pHYPERPO(q=0:x_max, mu=5, sigma=1.5)
+
+plot(x=0:x_max, y=cumulative_probs1, col="dodgerblue",
+     type="o", las=1, ylim=c(0, 1),
+     main="Cumulative probility for hyper-Poisson",
+     xlab="X", ylab="Probability")
+points(x=0:x_max, y=cumulative_probs2, type="o", col="tomato")
+points(x=0:x_max, y=cumulative_probs3, type="o", col="green4")
+legend("bottomright", col=c("dodgerblue", "tomato", "green4"), lwd=3,
+       legend=c("sigma=1.5, mu=1",
+                "sigma=1.5, mu=3",
+                "sigma=1.5, mu=5"))
+
+# Example 3
+# Some tests to compare the simulator with theoretical probabilities
+x_max <- 15
+probs1 <- dHYPERPO(x=0:x_max, mu=3, sigma=1.1)
+names(probs1) <- 0:x_max
+
+x <- rHYPERPO(n=10000, mu=3, sigma=1.1)
+probs2 <- prop.table(table(x))
+
+cn <- union(names(probs1), names(probs2))
+height <- rbind(probs1[cn], probs2[cn])
+nombres <- cn
+mp <- barplot(height, beside = TRUE, names.arg = nombres,
+              col=c('dodgerblue3','firebrick3'), las=1,
+              xlab='X', ylab='Proportion')
+legend('topright',
+       legend=c('Theoretical', 'Simulated'),
+       bty='n', lwd=3,
+       col=c('dodgerblue3','firebrick3'), lty=1)
+
