d88d4532e33889913b08ead71b4b1a42fac05a58
fhernanb <fhernanb@unal.edu.co>
2022-11-29 19:49:51 -0500

    Documentando las funciones internas

--- R/estim_mu_sigma_HYPERPO.R
+++ R/estim_mu_sigma_HYPERPO.R
@@ -1,10 +1,21 @@
+#' logLik function for hyper Poisson
+#' @description Calculates logLik for hyper Poisson distribution.
+#' @param logparam vector with parameters in log scale.
+#' @param x vector with the response variable.
+#' @keywords internal
+#' @export
 logLik_HYPERPO <- function(logparam=c(0, 0), x){
   return(sum(dHYPERPO(x     = x,
                       mu    = exp(logparam[1]),
                       sigma = exp(logparam[2]),
                       log=TRUE)))
 }
-
+#' Initial values for hyper Poisson
+#' @description This function generates initial values for the parameters.
+#' @param y vector with the response variable.
+#' @keywords internal
+#' @export
+#' @importFrom stats optim
 estim_mu_sigma_HYPERPO <- function(y) {
   mod <- optim(par=c(0, 0),
                fn=logLik_HYPERPO,
