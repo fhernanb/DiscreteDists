49808448b3b2f5e02a77c260a09ba2d0077faad9
fhernanb <fhernanb@unal.edu.co>
2023-09-21 19:38:36 -0500

    cambio en la plot cdf

--- R/plot_discrete_cdf.R
+++ R/plot_discrete_cdf.R
@@ -2,8 +2,10 @@
 #'
 #' @author Freddy Hernandez, \email{fhernanb@unal.edu.co}
 #'
-#' @param x vector with the values of the random variable.
-#' @param fx vector with the probabilities of x.
+#' @param x vector with the values of the random variable \eqn{X}.
+#' @param fx vector with the probabilities of \eqn{X}.
+#' @param col color for the line.
+#' @param lwd line width.
 #' @param ... further arguments and graphical parameters.
 #'
 #' @return
@@ -13,19 +15,15 @@
 #' @importFrom graphics abline points
 #' @export
 #'
-plot_discrete_cdf <- function(x, fx, ...) {
+plot_discrete_cdf <- function(x, fx,
+                              col="blue", lwd=3, ...) {
   Fx <- cumsum(fx)
-  n <- length(x)
+  F <- stepfun(x=x, y=c(0, Fx), right=TRUE)
 
-  plot(x = NA, y = NA, pch = NA,
-       xlim = c(0, max(x)),
-       ylim = c(0, 1), ...)
+  # Para dibujar la funcion F(x)
+  plot(F, verticals=FALSE,
+       lwd=lwd, col=col, las=1,
+       xlab="X", ylab="F(X=x)", main="")
 
-  abline(h=c(0, 1), col="grey", lty=2) # horizontal lines
-
-  points(x=x[-n], y=Fx[-1], pch=19)
-  points(x=x[-1], y=Fx[-1], pch=1)
-
-  for(i in 1:(n-1))
-    points(x=x[i+0:1], y=Fx[c(i,i)+1], type="l", ...)
+  grid()   # Para incluir una rejilla
 }
--- examples/examples_plot_cdf.R
+++ examples/examples_plot_cdf.R
@@ -1,11 +1,10 @@
 # Example 1
 # for a particular distribution
 
-x <- 0:6
-fx <- c(0, 0.19, 0.21, 0.4, 0.12, 0.05, 0.03)
+x <- 1:6
+fx <- c(0.19, 0.21, 0.4, 0.12, 0.05, 0.03)
 plot_discrete_cdf(x, fx, las=1,
-                  xlab="X",
-                  ylab="Probability")
+                  xlab="X", ylab="Probability")
 
 # Example 2
 # for a Poisson distribution
@@ -15,5 +14,3 @@ plot_discrete_cdf(x, fx, las=1,
                   xlab="X",
                   ylab="Probability",
                   main="CDF for Poisson")
-
-
